366a24c487a66a605bd3d363ab83f5fceea7b1c7
mmzgc776 <mmzgc776@gmail.com>
2020-06-15 12:40:09 -0500

    Avance en 3 funciones sobre usb_read_block

--- fineoffset.cpp
+++ fineoffset.cpp
@@ -1,6 +1,7 @@
 #ifndef usbhub
 #include <usbhub.h>
 #endif
+#include "usbhid.h"
 #include "fineoffset.h"
 #include <stdio.h>
 #include <string.h>
@@ -17,76 +18,6 @@ void DeviceReader::openPort(USB Usb)
   find_device(Usb);
 }
 
-uint8_t *DeviceReader::read_usb_block(USB Usb, int address)
-{
-  Serial.println("Iniciando lectura de usb: read_usb_block");
-  Serial.print("address: ");
-  Serial.println(address);
-  int ptr = address;
-  uint8_t usb_addr = 1; //Dirección en el usb host
-  uint8_t endPoint = 81; //81 en el descriptor de usb y script 0
-  uint8_t bmReqType = 33; //usb.TYPE_CLASS + usb.RECIP_INTERFACE en getData.py
-  uint8_t bRequest = 0x09;
-  uint8_t wValLo = 0x00;;
-  uint8_t wValHi = 0x02;
-  uint16_t wInd  = 0;
-  uint16_t total;
-
-  //Buffer a envíar a la estación
-  uint8_t rqstBuffer[8];
-  rqstBuffer[0] = 0xA1;               // READ COMMAND
-  rqstBuffer[1] = (char)(ptr / 256);  // READ ADDRESS HIGH
-  rqstBuffer[2] = (char)(ptr & 0xFF); // READ ADDRESS LOW
-  rqstBuffer[3] = 0x20;               // END MARK
-  rqstBuffer[4] = 0xA1;               // READ COMMAND
-  rqstBuffer[5] = (char)(ptr / 256);  // READ ADDRESS HIGH
-  rqstBuffer[6] = (char)(ptr & 0xFF); // READ ADDRESS LOW
-  rqstBuffer[7] = 0x20;               // END MARK
-  total = 8;                          //rqstBuffer.size()
-
-  uint16_t BufferSize = kBufferSize;
-  uint8_t readBuffer[BufferSize];
-  uint8_t *output = new uint8_t[BufferSize];
-  //Serial.println("requestBuffer:");
-  //Serial.println("Termina requestBuffer");
-  /*Usb.ctrlReq(usb_address,  //address
-                endPoint, //endPoint
-                321, //requestType
-                0x0000009, //request             
-                0, //wValLo
-                512, //wValHi
-                0, //wInd?
-                0x20, //total
-                sizeof(rqstBuffer)/sizeof(byte), //size?                
-                rqstBuffer, //data
-                NULL); //timeout o parser?*/
-  //uint16_t* parser = 0;
-  Serial.print(bmReqType);
-  Serial.print(bRequest);
-  for (int i =0; i<8;i++){    
-    Serial.print(rqstBuffer[i]);
-    Serial.print(" ");
-  }
-
-
-
-  Serial.println("Control Request");
-  Usb.ctrlReq(usb_addr, endPoint, bmReqType, bRequest, wValLo, wValHi, wInd, total, sizeof(rqstBuffer) / sizeof(byte), rqstBuffer, NULL);
-  Serial.println("In transfer");
-  Usb.inTransfer(usb_addr, endPoint, &BufferSize, readBuffer);
-  //Prueba de salida de una lectura unica
-  //Serial.print("ReadBuffer: ");
-  for (int i = 0; i < BufferSize; i++)
-  {
-    // Serial.print(" ");
-    // Serial.print(readBuffer[i]);
-    // Serial.print(" ");
-    output[i] = readBuffer[i];
-  }
-  //Serial.println(".");
-  return output;
-}
-
 //Find the vendor and product ID on the USB.
 void DeviceReader::find_device(USB Usb)
 {
@@ -97,15 +28,65 @@ void DeviceReader::find_device(USB Usb)
   Serial.println("Clase DeviceReader/find_device");
   if (Usb.getUsbTaskState() == USB_STATE_RUNNING)
   {
-    Serial.println("USB_STATE_RUNNING > read_block, ptr:" + 1);
+    Serial.println("USB_STATE_RUNNING > read_block, ptr:" + 1);    
     read_fixed_block(Usb);
   }
 }
+
+uint8_t *DeviceReader::read_fixed_block(USB Usb, int hi)
+{
+  Serial.println("Clase DeviceReader/read_fixed_block");
+  uint8_t *fixed_block = new uint8_t[kBufferSize];
+  uint8_t new_block[kBufferSize];
+
+  //Copiamos lo que devuelve read_block a fixed_block
+  //Que es block, el ejemplo de C muestra un puntero pero no sé que es?
+  //En el ejemplo en C, el uno es un cero  
+  for (int mempos = 0x0000; mempos<0x0100; mempos= mempos + 0x0020){
+    Serial.println("leyendo las posiciones.");
+    memcpy(new_block, read_block(Usb, mempos, true), kBufferSize);
+    for (int i = 0; i < kBufferSize; i++)
+      {
+        //Serial.print(i);
+        Serial.print(" ");
+        Serial.print(new_block[i]);
+        Serial.print(" ");
+      }
+      Serial.println(".");
+  }
+  // Check for valid magic numbers:
+  // This is hardly an exhaustive list and I can find no definitive
+  // documentation that lists all possible values; further, I suspect it is
+  // more of a header than a magic number...
+  if ((new_block[0] == 0x55) ||
+      (new_block[0] == 0xFF) ||
+      (new_block[0] == 0x01) ||
+      ((new_block[0] == 0x00) && (new_block[1] == 0x1E)) ||
+      ((new_block[0] == 0x00) && (new_block[1] == 0x01)))
+  {
+    //Retornar el valor del bloque, creo, hay que entender la funcion
+    Serial.println("Listo para retornar");
+    //return OK;
+    return fixed_block;
+  }
+  else
+  {
+    Serial.println("WH1080: readFixedBlock bad magic number %2.2X %2.2X");
+    Serial.println((int)new_block[0]);
+    Serial.println((int)new_block[1]);
+    Serial.println("WH1080: You may want to clear the memory on the station "
+                  "console to remove any invalid records or data...");
+    //Valio madres, no se leeyo correctamente, restablecer la estación
+    //return ERROR_ABORT;
+  }  
+}
+
 // Read block repeatedly until it's stable. This avoids getting corrupt
 // data when the block is read as the station is updating it.
 uint8_t *DeviceReader::read_block(USB Usb, int ptr, bool retry)
 {
-  bool flag = false;
+  Serial.println("Iniciando lectura de bloque: read_block");
+  bool readDone = false;
   uint8_t old_block[kBufferSize];
   uint8_t *new_block;
   //Llenamos el arreglo
@@ -151,12 +132,9 @@ uint8_t *DeviceReader::read_block(USB Usb, int ptr, bool retry)
         Serial.println("Entra a la comparacion");
         //Metodo con memcomp
         if (memcmp(old_block, new_block, 32) == 0)
-        {
-          // They match and we are done:
-          //
-          //readDone = TRUE;
+        {          
           Serial.println("Lecturas iguales, array igual");
-          flag = true;
+          readDone = true;
           break;
         }
         else
@@ -164,52 +142,86 @@ uint8_t *DeviceReader::read_block(USB Usb, int ptr, bool retry)
           Serial.println("WH1080: readBlock buffer still changing");
           //Serial.println("Elemento diferente");
           memcpy(old_block, new_block, 32);
-          flag = false;
-          //break;
+          readDone = false;
         }
       }
     }
   }
   return new_block;
 }
-uint8_t *DeviceReader::read_fixed_block(USB Usb)
-{
-  uint8_t *fixed_block = new uint8_t[kBufferSize];
-  uint8_t new_block[kBufferSize];
 
-  //Copiamos lo que devuelve read_block a fixed_block
-   //Que es block, el ejemplo de C muestra un puntero pero no sé que es?
-   //En el ejemplo en C, el uno es un cero  
-  memcpy(new_block, read_block(Usb, 0, true), kBufferSize); 
-      
-  //Llenamos el arreglo
-  // for (int i = 0; i < kBufferSize; i++)
-  // {
-  //   fixed_block[i] = 0;
-  // }
-  // Check for valid magic numbers:
-  // This is hardly an exhaustive list and I can find no definitive
-  // documentation that lists all possible values; further, I suspect it is
-  // more of a header than a magic number...
-  if ((new_block[0] == 0x55) ||
-      (new_block[0] == 0xFF) ||
-      (new_block[0] == 0x01) ||
-      ((new_block[0] == 0x00) && (new_block[1] == 0x1E)) ||
-      ((new_block[0] == 0x00) && (new_block[1] == 0x01)))
-  {
-    //Retornar el valor del bloque, creo, hay que entender la funcion
-    Serial.println("Listo para retornar");
-    //return OK;
-    return fixed_block;
+uint8_t *DeviceReader::read_usb_block(USB Usb, int address)
+{
+  Serial.println("Iniciando lectura de usb: read_usb_block");
+  //Serial.print("address: ");
+  //Serial.println(address);
+  int offset = address;
+  uint8_t usb_addr = 1; //Dirección en USB_desc 1
+  uint8_t endPoint = 0; //81 en el descriptor de usb y script 0 
+  uint8_t bmReqType = 33; //usb.TYPE_CLASS + usb.RECIP_INTERFACE en getData.py
+  uint8_t bRequest = 0x09;  //usb request, set config
+  uint8_t wValLo = 0x00;; 
+  uint8_t wValHi = 0x02;
+  uint16_t wInd  = 0;
+  uint16_t total = 8;  //rqstBuffer.size()
+
+  //Buffer a envíar a la estación
+  uint8_t rqstBuffer[8];
+  rqstBuffer[0] = 0xA1;               // READ COMMAND
+  rqstBuffer[1] = (char)(offset / 256);  // READ ADDRESS HIGH
+  rqstBuffer[2] = (char)(offset & 0xFF); // READ ADDRESS LOW
+  rqstBuffer[3] = 0x20;               // END MARK
+  rqstBuffer[4] = 0xA1;               // READ COMMAND
+  rqstBuffer[5] = (char)(offset / 256);  // READ ADDRESS HIGH
+  rqstBuffer[6] = (char)(offset & 0xFF); // READ ADDRESS LOW
+  rqstBuffer[7] = 0x20;               // END MARK  
+
+  uint16_t BufferSize = kBufferSize; //32
+  uint8_t readBuffer[BufferSize];
+  uint8_t *output = new uint8_t[BufferSize];
+
+  Serial.print(bmReqType);
+  Serial.print(", ");
+  Serial.print(bRequest);
+  Serial.print(", ");
+  for (int i =0; i<8;i++){    
+    Serial.print(rqstBuffer[i]);
+    Serial.print(", ");
   }
-  else
+  //Leer y descartar lo anterior, esto venía en el ejemplo en C
+  //( *( work->medium.usbhidRead ) )( &work->medium, newBuffer, 32, 500 );
+  Usb.inTransfer(usb_addr, endPoint, &BufferSize, readBuffer);
+  Serial.println("Control Request");
+  uint8_t result;
+  result = Usb.ctrlReq(usb_addr, //Address
+            endPoint, //usb Endpoint
+            bmReqType, //bmReqType, //33 (ejemplo ps3 bmREQ_HID_OUT, es similar al de wview )
+            bRequest, //09 set configuration
+            wValLo, //bConfigurationValue 0x0000200: 00 02 o 02 00
+            wValHi, //bConfigurationValue este on el de arriba
+            wInd, //siempre es 0 inguesu
+            total, //bytes?
+            sizeof(rqstBuffer) / sizeof(byte), //bytes a leer
+            rqstBuffer, //puntero del bufer de request
+            NULL);  //Timeout?
+  Serial.println(result, HEX);
+  Serial.println("In transfer");
+  result = Usb.inTransfer(usb_addr, endPoint, &BufferSize, readBuffer);
+  Serial.println(result, HEX);
+  //Prueba de salida de una lectura unica
+  Serial.print("ReadBuffer: ");
+  for (int i = 0; i < BufferSize; i++)
   {
-    Serial.println("WH1080: readFixedBlock bad magic number %2.2X %2.2X");
-    Serial.println((int)new_block[0]);
-    Serial.println((int)new_block[1]);
-    Serial.println("WH1080: You may want to clear the memory on the station "
-                  "console to remove any invalid records or data...");
-    //Valio madres, no se leeyo correctamente, restablecer la estación
-    //return ERROR_ABORT;
-  }  
+    // Serial.print(" ");
+    // Serial.print(readBuffer[i]);
+    // Serial.print(" ");
+    output[i] = readBuffer[i];
+  }
+  //Serial.println(".");
+  return output;
 }
+
+
+
+
+
--- fineoffset.h
+++ fineoffset.h
@@ -5,10 +5,14 @@
 #include <stdio.h>
 #include <string.h>
 
+//Tratare de implementar WH1080_WORK en mi implementación
+  // define the readings collector
 
+#define WH1080_BUFFER_CHUNK         0x20        // Size of chunk received over USB
 class DeviceReader
 {
 public:
+  //int PERADDR = 0xe0;
   DeviceReader();
   int ciclos = 0;  
   int vendor_id = 1941;
@@ -16,9 +20,9 @@ public:
   int usb_endpoint = 81; // revisar para que sirve, evitar confusión
   //int usb_addr = 1; //dirección del dispositivo en el hub???
   //int address = 1; //Direccion en memoria de la estacion
-  
   const uint16_t kBufferSize = 32; //Esto estaba definido como 10, pero luego lo cambié a 16 observando la script de getData.py
-   //Abrimos el puerto y lo dejamos disponible, basicamente llamamos a openPort()
+  
+  //Abrimos el puerto y lo dejamos disponible, basicamente llamamos a openPort()
   void openPort(USB Usb);
   //Se supone que esta función buscaría el puerto y direccion, pero al ser un solo adaptador se puede obviar (creo), dejando 
   //las direcciones predeterminadas, revisa que esté listo el USB
@@ -32,5 +36,6 @@ public:
   //Funcion siguiente read_block, lee de 20 en 20 retorna un arreglo 
   //cuando está correcto, usa los magic_numbers para validar
   //Ptr se llama block en el ejemplo de C, pero ptr en el ejemplo de python
-  uint8_t* read_fixed_block(USB Usb);
+  uint8_t* read_fixed_block(USB Usb, int hi = 0x0100);  
+
 };
